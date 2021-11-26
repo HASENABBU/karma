10d30179079d352ed584a47c47b10b7f489a877b
HASENABBU <hasen.aero2010@gmail.com>
2021-11-26 17:00:04 +0530

    message

---
+++ src/org/college/College.java
@@ -0,0 +1,23 @@
+package org.college;
+
+public class College {
+private void colleageName() {
+	// TODO Auto-generated method stub
+System.out.println("colleage name is bharath");
+}
+private void collegeCode() {
+	// TODO Auto-generated method stub
+System.out.println("college code is 9256");
+}
+private void colleageRank() {
+	// TODO Auto-generated method stub
+System.out.println("colleage rank is 75657");
+}
+public static void main(String[] args) {
+	College z=new College();
+	z.colleageName();
+	z.collegeCode();
+	z.colleageRank();
+}
+}
+
